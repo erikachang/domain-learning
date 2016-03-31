a735452d256083753f954ddf35aa3db7ea40b2ee
Stephan Chang <stchepanhagn@gmail.com>
2016-03-31 17:47:25 -0300

    Remove call to python from tests.py

--- tests.py
+++ tests.py
@@ -15,8 +15,6 @@ You should have received a copy of the GNU General Public License
 along with this program, located in the root of this repository.
 If not, see <http://www.gnu.org/licenses/>.
 """
-#!/usr/bin/python
-
 import unittest
 import planning as p
 
